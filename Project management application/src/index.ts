c4d3a2d49a56c25f5d27465365949f16d9b6406c
viachaskul <fixrapdoK@gmail.com>
2022-11-15 02:59:30 +0300

    feat: add mongo connection

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://admin:<password>@cluster0.muiqf78.mongodb.net/test');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
